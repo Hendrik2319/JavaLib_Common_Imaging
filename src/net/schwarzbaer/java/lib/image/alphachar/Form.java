1243ebca22771f1cb8944d3d8e4f957592325d57
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-07-27 12:26:19 +0200

    feat: added a new set method to image.alphachar.Form.PolyLine.Point

--- src/net/schwarzbaer/java/lib/image/alphachar/Form.java
+++ src/net/schwarzbaer/java/lib/image/alphachar/Form.java
@@ -59,7 +59,8 @@ public interface Form {
 			public double x,y;
 			public Point(double x, double y) { set(x,y); }
 			public void set(double x, double y) { this.x = x; this.y = y; }
-			public void set(Point2D.Float p) { this.x = p.x; this.y = p.y; }
+			public void set(Point2D.Float  p) { this.x = p.x; this.y = p.y; }
+			public void set(Point2D.Double p) { this.x = p.x; this.y = p.y; }
 		}
 	}
 	
